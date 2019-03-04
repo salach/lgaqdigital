eef60e049cea2efabda77df70f96aef3d46f3e3b
salina <salina.acharya@ladbrokes.com.au>
2019-03-05 09:15:33 +1000

    Initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,5 @@
+$(".back-to-top").find('a').click(function(e) {
+    e.preventDefault();
+    var aid = $(this).attr("href");
+    $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+});
---
+++ dist/scripts/main.js
@@ -0,0 +1,2 @@
+$(".back-to-top").find("a").click(function(t){t.preventDefault();var o=$(this).attr("href");$("html,body").animate({scrollTop:$(o).offset().top},"slow")});
+//# sourceMappingURL=main.js.map
